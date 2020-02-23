0565a6767251d44f3a17ef57fc30dfbbf6fc1d56
saint-prince <luckyericliu@gmail.com>
2020-02-23 10:13:02 +0300

    Entities defined

---
+++ post-service/src/main/java/com/sakinramazan/microservices/postservice/entity/Comment.java
@@ -0,0 +1,28 @@
+package com.sakinramazan.microservices.postservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode
+@Entity(name = "blog")
+public class Comment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String writer;
+
+    @ManyToOne
+    @JoinColumn(name = "blog_id", referencedColumnName = "id")
+    private Post post;
+
+}
