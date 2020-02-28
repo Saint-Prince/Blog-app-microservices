e60aa7b1d233cd661e68e07601d024717dab9b93
saint-prince <luckyericliu@gmail.com>
2020-02-28 22:18:26 +0300

    blog service eureka client pom & annos & props added

--- blog-service/src/main/java/com/sakinramazan/microservices/blogservice/BlogServiceApplication.java
+++ blog-service/src/main/java/com/sakinramazan/microservices/blogservice/BlogServiceApplication.java
@@ -2,7 +2,15 @@ package com.sakinramazan.microservices.blogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
 public class BlogServiceApplication {
 
