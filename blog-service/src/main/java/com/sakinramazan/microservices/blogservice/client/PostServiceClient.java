d00fce2f730af2deade8ebc3f844be1b08120e83
saint-prince <luckyericliu@gmail.com>
2020-02-28 22:22:18 +0300

    blog service pom fix on feign client as post service

--- blog-service/src/main/java/com/sakinramazan/microservices/blogservice/client/PostServiceClient.java
+++ blog-service/src/main/java/com/sakinramazan/microservices/blogservice/client/PostServiceClient.java
@@ -1,7 +1,7 @@
 package com.sakinramazan.microservices.blogservice.client;
 
 import com.sakinramazan.microservices.blogservice.entity.Post;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 @FeignClient("post-service")
 public interface PostServiceClient {
 
-    @PostMapping(value = "/post")
+    @PostMapping(value = "/")
     Post savePost(@Valid @RequestBody Post post);
 
 }
