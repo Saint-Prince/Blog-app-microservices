6c11a63c3a3813fdcffc07af53627ce779dd6367
saint-prince <luckyericliu@gmail.com>
2020-02-28 21:36:09 +0300

    Eureka client emergency warnig fix

--- comment-service/src/main/java/com/sakinramazan/microservices/commentservice/CommentServiceApplication.java
+++ comment-service/src/main/java/com/sakinramazan/microservices/commentservice/CommentServiceApplication.java
@@ -2,12 +2,20 @@ package com.sakinramazan.microservices.commentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
 public class CommentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommentServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommentServiceApplication.class, args);
+    }
 
 }
