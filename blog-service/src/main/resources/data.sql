aa9e96b0f7d5ac1ceefd004b526ebc603c7e7da2
saint-prince <luckyericliu@gmail.com>
2020-02-28 22:48:58 +0300

    Sample test datas added

---
+++ blog-service/src/main/resources/data.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS comment;
+
+DROP TABLE IF EXISTS post;
+
+DROP TABLE IF EXISTS blog;
+
+CREATE TABLE blog(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    subject varchar(80) NOT NULL
+);
+
+CREATE TABLE post(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    writer varchar(50) NOT NULL,
+    blog_id INT,
+    FOREIGN KEY (blog_id) REFERENCES blog(id)
+);
+
+CREATE TABLE comment(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    writer varchar(50) NOT NULL,
+    detail varchar(150) NOT NULL,
+    post_id INT,
+    FOREIGN KEY (post_id) REFERENCES post(id)
+);
+
+-- ////////////////////////////////////////////////////////////////////////////
+--  Sample static test values
+-- ////////////////////////////////////////////////////////////////////////////
+INSERT INTO blog(subject) VALUES    ('Sample Blog 1'),
+                                    ('Sample Blog 2'),
+                                    ('Sample Blog 3');
+
+
+INSERT INTO post(writer, blog_id) VALUES    ('Sample writer-1', 1),
+                                            ('Sample writer-2', 1),
+                                            ('Sample writer-3', 1),
+                                            ('Sample writer-1', 2),
+                                            ('Sample writer-5', 3);
+
+INSERT INTO comment(writer, detail, post_id)    ('Sample writer-1', 'Sample comment detail-1', 1),
+                                                ('Sample writer-2', 'Sample comment detail-2', 1),
+                                                ('Sample writer-1', 'Sample comment detail-3', 3),
+                                                ('Sample writer-2', 'Sample comment detail-4', 3),
+                                                ('Sample writer-2', 'Sample comment detail-5', 4),
+                                                ('Sample writer-3', 'Sample comment detail-6', 1),
+                                                ('Sample writer-1', 'Sample comment detail-7', 2),
+                                                ('Sample writer-1', 'Sample comment detail-8', 2);
+
