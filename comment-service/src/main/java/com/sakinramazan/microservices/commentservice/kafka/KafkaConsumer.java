bfffe4e5edfdf219db8807b95dbe1878f1394abb
saint-prince <luckyericliu@gmail.com>
2020-02-29 13:31:53 +0300

    comment-service configured as Kafka client

---
+++ comment-service/src/main/java/com/sakinramazan/microservices/commentservice/kafka/KafkaConsumer.java
@@ -0,0 +1,24 @@
+package com.sakinramazan.microservices.commentservice.kafka;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    @KafkaListener(topics = "Sample Topic")
+    public void receiveMessage(String message) {
+        LOGGER.info("received message='{}'", message);
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}
