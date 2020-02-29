0851157a5c5589e1b1c478177cfcab9259e94b12
saint-prince <luckyericliu@gmail.com>
2020-02-29 07:44:05 +0300

    adding some central configs to test

--- config-server/src/main/java/com/sakinramazan/microservices/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/sakinramazan/microservices/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.sakinramazanmicroservices.configserver;
+package com.sakinramazan.microservices.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
