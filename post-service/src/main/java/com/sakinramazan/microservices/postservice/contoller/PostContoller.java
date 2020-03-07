244638fc6fbc2897c983ca42856378ef598717f1
saint-prince <luckyericliu@gmail.com>
2020-03-07 09:24:29 +0300

    Caching mechanism via Redis added to Post service to show sample usage

--- post-service/src/main/java/com/sakinramazan/microservices/postservice/contoller/PostContoller.java
+++ post-service/src/main/java/com/sakinramazan/microservices/postservice/contoller/PostContoller.java
@@ -5,6 +5,9 @@ import com.sakinramazan.microservices.postservice.entity.Post;
 import com.sakinramazan.microservices.postservice.kafka.KafkaProducer;
 import com.sakinramazan.microservices.postservice.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +31,8 @@ public class PostContoller {
         return new ResponseEntity<>(postService.getAllPosts(), HttpStatus.OK);
     }
 
+    // Conditional Caching - If the incoming post's subject is not 'Sample subject 1', cache it
+    @Cacheable(value = "posts", key = "#id", unless = "#result.body.blog.subject != 'Sample subject 1'")
     @GetMapping("/{id}")
     public ResponseEntity<Post> getPostById(@PathVariable(value = "id") Integer id) {
         return ResponseEntity.ok().body(postService.getPost(id));
@@ -38,11 +43,15 @@ public class PostContoller {
         return new ResponseEntity<>(postService.createPost(post), HttpStatus.CREATED);
     }
 
+    // CachePut for not deleting , just adding new one
+    @CachePut(value = "posts", key = "#post.id")
     @PutMapping("/")
     public ResponseEntity<Post> updatePost(@Valid @RequestBody Post post) {
         return new ResponseEntity<>(postService.updatePost(post), HttpStatus.OK);
     }
 
+    // CacheEvit for delete all first then put new ones
+    @CacheEvict(value = "posts", allEntries = true)
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deletePost(@PathVariable(value = "id") Integer id) {
         Post post = postService.getPost(id);
