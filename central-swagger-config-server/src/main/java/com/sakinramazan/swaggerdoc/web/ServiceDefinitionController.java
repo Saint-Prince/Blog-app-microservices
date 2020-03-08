4d110e2099d4e05e548a93ac487043224d5a4c19
saint-prince <luckyericliu@gmail.com>
2020-03-08 15:25:06 +0300

    comment service - data.sql added & clean up

---
+++ central-swagger-config-server/src/main/java/com/sakinramazan/swaggerdoc/web/ServiceDefinitionController.java
@@ -0,0 +1,21 @@
+package com.sakinramazan.swaggerdoc.web;
+
+import com.sakinramazan.swaggerdoc.config.swagger.ServiceDefinitionsContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("swaggerdocs")
+public class ServiceDefinitionController {
+
+    @Autowired
+    private ServiceDefinitionsContext definitionContext;
+
+    @GetMapping("/service/{servicename}")
+    public String getServiceDefinition(@PathVariable("servicename") String serviceName) {
+        return definitionContext.getSwaggerDefinition(serviceName);
+    }
+}
