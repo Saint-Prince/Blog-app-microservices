8a3659390bb24cdfe53015a1506b45f50824c19d
saint-prince <luckyericliu@gmail.com>
2020-03-15 13:16:33 +0300

    zuul & security configs up

--- api-gateway/src/main/java/com/sakinramazan/microservices/apigateway/config/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/sakinramazan/microservices/apigateway/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,7 +19,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter implemen
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().headers().frameOptions().sameOrigin().httpStrictTransportSecurity().disable();
+        http
+                .cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
     }
 
     @Override
